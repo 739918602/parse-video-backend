86f89c83676ab82de927aa2b3a777afa4d176978
zys <zys@qq.com>
2024-11-18 16:09:47 +0800

    add cors

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/wujunwei928/parse-video/parser"
 )
@@ -26,6 +27,15 @@ var files embed.FS
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	sub, err := fs.Sub(files, "templates")
 	if err != nil {
